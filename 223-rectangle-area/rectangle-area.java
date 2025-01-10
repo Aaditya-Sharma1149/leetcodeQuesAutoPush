9b4fedd789184dbfc4ce9ba2127b68b7b0e2851d
Aaditya-Sharma1149 <87012582+Aaditya-Sharma1149@users.noreply.github.com>
2025-01-10 20:16:58 +0530

    Time: 1 ms (100.00%) | Memory: 42.4 MB (79.48%) - LeetSync

---
+++ 223-rectangle-area/rectangle-area.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int cx1=Math.max(ax1,bx1);
+        int cy1=Math.max(ay1,by1);
+        int cx2=Math.min(ax2,bx2);
+        int cy2=Math.min(ay2,by2);
+        if(cx1<cx2 && cy1<cy2){
+            return (ax2-ax1)*(ay2-ay1) + (bx2-bx1)*(by2-by1) - (cx2-cx1)*(cy2-cy1);
+        }
+        return (ax2-ax1)*(ay2-ay1) + (bx2-bx1)*(by2-by1);
+    }
+}

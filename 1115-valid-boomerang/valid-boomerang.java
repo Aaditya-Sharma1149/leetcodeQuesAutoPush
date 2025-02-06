467d5345512c4268b0c8985b912d0d007da5115c
Aaditya-Sharma1149 <87012582+Aaditya-Sharma1149@users.noreply.github.com>
2025-02-06 22:33:29 +0530

    Time: 8 ms (1.40%) | Memory: 42.1 MB (7.80%) - LeetSync

---
+++ 1115-valid-boomerang/valid-boomerang.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isBoomerang(int[][] points) {
+        //different slope
+        if( (points[0][0]==points[1][0]) || (points[0][0]==points[2][0]) ){
+            if( (points[0][0]==points[1][0]) && (points[0][0]==points[2][0]) ){
+                return false;
+            }
+            //y1=y2 or y1=y3    any one is repeting point
+            if( ((points[0][0]==points[1][0]) && (points[0][1]==points[1][1])) || ((points[0][0]==points[2][0]) && (points[0][1]==points[2][1])) ) return false;
+            return true;
+        }
+        double slopeAB = (double) (points[0][1]-points[1][1]) / (points[0][0]-points[1][0]);
+        double slopeAC = (double) (points[0][1]-points[2][1]) / (points[0][0]-points[2][0]); //1-2/0-2
+        if ( slopeAB == slopeAC ){
+            System.out.println(slopeAC);
+            System.out.println((points[0][1] + "-" +points[2][1]) + "=" + (points[0][1]-points[2][1]));
+            System.out.println( (points[0][0]+ "-" +points[2][0]) + "=" + (points[0][0]-points[2][0]));
+            System.out.println((points[0][1]-points[2][1]) / (points[0][0]-points[2][0]));
+            return false;
+        }
+        return true;
+    }
+}

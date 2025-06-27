43cf4fefae6a587ddd3402e0b20d38ae17ee89a7
Aaditya-Sharma1149 <87012582+Aaditya-Sharma1149@users.noreply.github.com>
2025-06-27 10:23:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (95.25%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -10,7 +10,6 @@ class Solution {
         for(int i=1; i<m; i++){
             for(int j=1; j<n; j++){
                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                System.out.println(dp[i][j]);
             }
         }
         return dp[m-1][n-1];

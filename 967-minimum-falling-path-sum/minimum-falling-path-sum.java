6f6d88a78cd5fda62092c9948265971d258f174b
Aaditya-Sharma1149 <87012582+Aaditya-Sharma1149@users.noreply.github.com>
2025-06-27 23:20:20 +0530

    Time: 5 ms (75.35%) | Memory: 45.7 MB (19.29%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -2,27 +2,45 @@ class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int ans = Integer.MAX_VALUE;
         int [][] dp = new int[matrix.length][matrix[0].length];
+
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[0].length; j++){
                 dp[i][j]=Integer.MAX_VALUE;
             }
         }
-        for(int i=0; i<matrix[0].length; i++){
-            ans = Math.min(helper(matrix,0,i,dp),ans);
+        for(int j=0; j<dp[0].length; j++){
+            dp[0][j]=matrix[0][j]; 
+        }
+
+        for(int i=1; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+
+                int left; int right; int up;
+
+                if( j==0 )  left = Integer.MAX_VALUE;
+                else{
+                    left = dp[i-1][j-1];
+                }
+
+                if( j>matrix[0].length-2 ) right = Integer.MAX_VALUE;
+                else{
+                    right = dp[i-1][j+1];
+                }
+
+                up = dp[i-1][j];
+
+                dp[i][j]=Math.min(Math.min(left,right),up)+matrix[i][j];
+            }
+        }
+        // for(int r=0; r<dp.length; r++){
+        //     for(int c=0; c<dp[0].length; c++){
+        //         System.out.print(dp[r][c]);
+        //     }
+        //     System.out.println();
+        // }
+        for(int i=0; i<dp[0].length; i++){
+            ans = Math.min(ans,dp[matrix.length-1][i]);
         }
         return ans;
-    }
-    private int helper(int [][] matrix, int r, int c,int [][] dp){
-        if(c<0) return Integer.MAX_VALUE;
-        if(c>=matrix[0].length) return Integer.MAX_VALUE;
-        if(r>=matrix.length) return 0;
-        
-        if(dp[r][c]!=Integer.MAX_VALUE) return dp[r][c];
-
-        int left = helper(matrix,r+1,c-1,dp);
-        int bottom = helper(matrix,r+1,c,dp);
-        int right = helper(matrix,r+1,c+1,dp);
-
-        return dp[r][c] = Math.min(Math.min(left,right),bottom)+matrix[r][c];
     }
 }

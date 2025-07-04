741fe7af328bff5fabf7e0b141b408009392842d
Aaditya-Sharma1149 <87012582+Aaditya-Sharma1149@users.noreply.github.com>
2025-07-04 11:35:30 +0530

    Time: 27 ms (86.30%) | Memory: 51.5 MB (58.23%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,39 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List <List <Integer>> list = new ArrayList<List <Integer>>();
+        List<List<Integer>> list = new ArrayList<>();
         for(int i=0; i<nums.length; i++){
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
             if(nums[i]>0) break;
-            List <List<Integer>> ans = twoSum(nums,i);
-            if(!ans.isEmpty()){
-                list.addAll(twoSum(nums,i));
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int target = -nums[i];
+            int l = i+1;
+            int r = nums.length-1;
+            while(l<r){
+                int sum = nums[l] + nums[r];
+                if( sum == target){
+                    list.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                    l++;
+                    r--;
+                    while (l < r && nums[l] == nums[l - 1]) l++;
+                    while (l < r && nums[r] == nums[r + 1]) r--;
+                }
+                else if( sum > target){
+                    r--;
+                }
+                else{
+                    l++;
+                }
             }
-        }
-        if(list.isEmpty()){
-            return new ArrayList<>();
         }
         return list;
     }
-    private List<List<Integer>> twoSum(int[] nums,int idx){
-        int target = -nums[idx];
-        List<List<Integer>> list = new ArrayList<>();
-        HashMap <Integer,Integer> map = new HashMap<>();
-        Set <String> set= new HashSet<>();
-        for(int i=idx+1; i<nums.length; i++){
-            if(map.containsKey(nums[i])){
-                int a = nums[idx];
-                int b = nums[i];
-                int c =target-nums[i];
-                int [] triplet = new int[]{a,b,c};
-                Arrays.sort(triplet);
-                if(!set.contains(triplet[0] + " , " + triplet[1] + " , " + triplet[2])){
-                    set.add(triplet[0] + " , " + triplet[1] + " , " + triplet[2]);
-                    list.add(Arrays.asList(triplet[0],triplet[1],triplet[2]));
-                }
-            }
-            map.put(target-nums[i], i);
-        }
-        return list;
-    } 
 }

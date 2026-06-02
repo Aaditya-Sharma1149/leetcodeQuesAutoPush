d4fe2a2f74a1e0c57f68b0a11490ea0817e37f6d
Aaditya-Sharma1149 <87012582+Aaditya-Sharma1149@users.noreply.github.com>
2026-06-02 10:58:43 +0530

    Time: 6 ms (8.73%) | Memory: 47.1 MB (9.92%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
@@ -1,21 +1,29 @@
 class Solution {
     public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
-        int l = landStartTime.length;
-        int w = waterStartTime.length;
-        int time = Integer.MAX_VALUE; 
-        for(int i=0; i<l; i++){
-            int landTime = landStartTime[i] + landDuration[i];
-            for(int j=0; j<w; j++){
-                time = Math.min(time,Math.max(landTime,waterStartTime[j]) + waterDuration[j]);
-            }
+        int n = landStartTime.length;
+        int m = waterStartTime.length;
+        int time = Integer.MAX_VALUE;
+
+        int [] landTime = new int [n];
+        int [] waterTime = new int[m];
+        for(int i=0; i<n; i++){
+            landTime[i] = landStartTime[i] + landDuration[i];
         }
-        for(int i=0; i<w; i++){
-            int waterTime = waterStartTime[i] + waterDuration[i];
-            for(int j=0; j<l; j++){
-                time = Math.min(time,Math.max(waterTime,landStartTime[j]) + landDuration[j]);
-            }
+        Arrays.sort(landTime);
+        int minLandTime = landTime[0];
+        for(int i=0; i<m; i++){
+            waterTime[i] = waterStartTime[i] + waterDuration[i];
+        }
+        Arrays.sort(waterTime);
+        int minWaterTime =waterTime[0];
+        for(int i=0; i<m; i++){
+            time = Math.min(time,Math.max(minLandTime,waterStartTime[i]) + waterDuration[i]);
+        }
+        for(int i=0; i<n; i++){
+            time = Math.min(time,Math.max(minWaterTime,landStartTime[i]) + landDuration[i]);
         }
         return time;
+        
 
     }
 }

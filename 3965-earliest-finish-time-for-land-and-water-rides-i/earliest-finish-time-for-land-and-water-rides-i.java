df72d59dd3853397c3a134f088caf1e25578fee2
Aaditya-Sharma1149 <87012582+Aaditya-Sharma1149@users.noreply.github.com>
2026-06-02 11:02:41 +0530

    Time: 1 ms (100.00%) | Memory: 46.7 MB (77.78%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.java
@@ -3,19 +3,25 @@ class Solution {
         int n = landStartTime.length;
         int m = waterStartTime.length;
         int time = Integer.MAX_VALUE;
-
         int [] landTime = new int [n];
         int [] waterTime = new int[m];
+
         for(int i=0; i<n; i++){
             landTime[i] = landStartTime[i] + landDuration[i];
         }
-        Arrays.sort(landTime);
         int minLandTime = landTime[0];
+        for(int i=0;i<n;i++){
+            minLandTime=Math.min(minLandTime,landTime[i]);
+        }
+
         for(int i=0; i<m; i++){
             waterTime[i] = waterStartTime[i] + waterDuration[i];
         }
-        Arrays.sort(waterTime);
         int minWaterTime =waterTime[0];
+        for(int i=0;i<m;i++){
+            minWaterTime=Math.min(minWaterTime,waterTime[i]);
+        }
+        
         for(int i=0; i<m; i++){
             time = Math.min(time,Math.max(minLandTime,waterStartTime[i]) + waterDuration[i]);
         }

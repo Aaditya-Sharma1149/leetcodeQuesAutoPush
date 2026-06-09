f31d5cef9e9c96b45f695d4e7c090c12e6232ca1
Aaditya-Sharma1149 <87012582+Aaditya-Sharma1149@users.noreply.github.com>
2026-06-09 21:30:50 +0530

    Time: 188 ms (7.38%) | Memory: 48.7 MB (5.09%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -15,17 +15,23 @@
  */
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
-        if(root==null || (root.right==null && root.left==null)) return 0;
-        int leftAns = diameterOfBinaryTree(root.left);
-        int rightAns = diameterOfBinaryTree(root.right);
-        int ans=height(root.left)+height(root.right);
-        if(root.left!=null) ans++;
-        if(root.right!=null) ans++;
-        int max = Math.max(ans,Math.max(leftAns,rightAns));
-        return max;
+        int ans = 0;
+        int left = 0;
+        int right = 0;
+        if(root == null){
+            return 0;
+        }
+        if(root.left != null){
+            left = diameterOfBinaryTree(root.left);
+        }
+        if(root.right != null){
+            right = diameterOfBinaryTree(root.right);
+        }
+        ans = Math.max(dfs(root.left) + dfs(root.right) ,Math.max(left,right));
+        return ans;
     }
-    public int height(TreeNode root){
-        if(root==null || (root.right==null && root.left==null)) return 0;
-        return 1+Math.max(height(root.left),height(root.right));
+    public int dfs(TreeNode root){
+        if(root == null) return 0;
+        return Math.max(dfs(root.left),dfs(root.right)) + 1;
     }
 }

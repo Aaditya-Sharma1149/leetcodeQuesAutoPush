dc2cb7bea8018d72570fe0a9b96f7e419a7bdeb8
Aaditya-Sharma1149 <87012582+Aaditya-Sharma1149@users.noreply.github.com>
2026-08-02 14:16:13 +0530

    Time: 23 ms (91.68%) | Memory: 72.8 MB (25.64%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,28 +1,19 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int sum = sum(nums);
-        if(sum%2!=0) return false;
-        sum = sum/2;
-        boolean [][] dp = new boolean [nums.length+1][sum+1];
-        for(int i=0; i<dp.length; i++){
-            for(int j=0; j<dp[0].length; j++){
-                dp[i][j] = true;
-            }
-        }
-        return helper(nums,sum,0,dp);
-
-    }public int sum(int [] nums){
         int sum = 0;
-        for(int n:nums){
-            sum += n;
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
         }
-        return sum;
+        if(sum%2!=0) return false;
+        Boolean [][] dp = new Boolean [nums.length][sum/2+1];
+        return helper(nums,0,sum/2,0,dp);
     }
-    public boolean helper(int [] nums, int target, int idx, boolean [][] dp){
+    public boolean helper(int [] nums, int currSum, int target, int idx, Boolean [][]dp){
+        if(currSum == target) return true;
+        if(currSum > target) return false;
         if(idx==nums.length) return false;
-        if ( target<0 ) return false;
-        if(dp[idx][target] == false) return false;
-        if ( target==0 ) return true;
-        return dp[idx][target] = helper(nums, target-nums[idx], idx+1,dp) || helper(nums, target, idx+1,dp);
+        if(dp[idx][currSum]!=null) return dp[idx][currSum];
+        return  dp[idx][currSum] = helper(nums,currSum+nums[idx],target,idx+1,dp) || helper(nums,currSum,target,idx+1,dp);
+
     }
 }

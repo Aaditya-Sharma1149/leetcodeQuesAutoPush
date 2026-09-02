dbcd29d1a87fcc7441d2023e14157390e9f214ff
Aaditya-Sharma1149 <87012582+Aaditya-Sharma1149@users.noreply.github.com>
2026-09-02 12:50:57 +0530

    Time: 161 ms (34.87%) | Memory: 200.5 MB (5.41%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,6 +1,9 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int [][] dp = new int [target+40000][nums.length];
+        for(int i=0; i<dp.length; i++){
+            Arrays.fill(dp[i],-1);
+        }
         return helper(nums,target+20000,0,dp);
     }
     public int helper(int [] nums, int sum, int index, int [][] dp){
@@ -8,7 +11,7 @@ class Solution {
             if(sum==20000) return 1;
             return 0;
         }
-        if(dp[sum][index]!=0) return dp[sum][index];
+        if(dp[sum][index]!=-1) return dp[sum][index];
         return dp[sum][index] = helper(nums,sum-nums[index],index+1,dp) + helper(nums,sum+nums[index],index+1,dp);
     }
 }

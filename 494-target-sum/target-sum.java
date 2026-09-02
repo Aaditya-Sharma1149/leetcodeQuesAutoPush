7d3074fdd4fdbed8ecfd2f2d2364cfda18f6c1ba
Aaditya-Sharma1149 <87012582+Aaditya-Sharma1149@users.noreply.github.com>
2026-09-02 12:45:41 +0530

    Time: 464 ms (29.62%) | Memory: 200.1 MB (5.41%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,26 +1,14 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int sum = 0;
-        for(int n:nums){
-            sum += Math.abs(n);
-        }
-        int [][] dp  = new int[nums.length][2*sum+1];
-        for(int i=0; i<dp.length; i++){
-            for(int j=0; j<dp[0].length; j++){
-                dp[i][j]=-1;
-            }
-        }
-        return helper(nums, target, 0,dp,sum);
-
-    }public int helper(int [] nums, int target, int idx, int [][] dp, int sum){
-        if(target<-1*sum || target>sum) return 0;
-        if(idx==nums.length){
-            if(target==0) return 1;
+        int [][] dp = new int [target+40000][nums.length];
+        return helper(nums,target+20000,0,dp);
+    }
+    public int helper(int [] nums, int sum, int index, int [][] dp){
+        if(index==nums.length){
+            if(sum==20000) return 1;
             return 0;
         }
-        if(dp[idx][sum + target]!=-1){
-            return dp[idx][sum + target];
-        }
-        return dp[idx][sum + target] = helper(nums, target-nums[idx], idx+1,dp, sum) + helper(nums, target+nums[idx], idx+1,dp,sum);
+        if(dp[sum][index]!=0) return dp[sum][index];
+        return dp[sum][index] = helper(nums,sum-nums[index],index+1,dp) + helper(nums,sum+nums[index],index+1,dp);
     }
 }
